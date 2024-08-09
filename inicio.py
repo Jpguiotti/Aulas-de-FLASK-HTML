0bad60eddf44af14397ab27c1a047dead165af86
Jpguiotti <jpguiotti@gmail.com>
2024-08-09 17:46:33 -0300

    Add files via upload

---
+++ inicio.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from matematica import Matematica
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def ola():
+    return "Olá Mundo!"
+
+@app.route('/olamundo')
+def mostrar():
+    return render_template('pagina.html')
+
+
+@app.route('/calculosoma')
+def calcular():
+    mat = Matematica(5,7)
+    resposta = mat.somar()
+    return render_template('calculo.html', resultado=resposta)
+
+app.run()
+
